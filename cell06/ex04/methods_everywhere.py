cea91934ee98a947fde673f2fbf16ca15e622848
HESPERzLabs <jermta29@gmail.com>
2025-09-17 15:12:04 +0700

    Add cello6

---
+++ cell06/ex04/methods_everywhere.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import sys
+
+def shrink(s):
+    return s[:8]
+
+def enlarge(s):
+    return s + "Z" * (8 - len(s))
+
+if len(sys.argv) < 2:
+    print("none")
+else:
+    for arg in sys.argv[1:]:
+        if len(arg) > 8:
+            print(shrink(arg))
+        elif len(arg) < 8:
+            print(enlarge(arg))
+        else:
+            print(arg)
---
+++ cell06/ex05/scope_that.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def add_one(x):
+    return x + 1
+
+num = 1
+print("Before:", num)
+
+add_one(num)
+
+print("After:", num)
