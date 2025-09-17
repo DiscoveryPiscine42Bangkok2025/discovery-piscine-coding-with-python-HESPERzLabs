26ef48cca14c575f279fc082e89d73d7d6265beb
HESPERzLabs <jermta29@gmail.com>
2025-09-17 10:42:41 +0700

    Add i_got_that.py

--- cell03/ex02/i_got_that.py
+++ cell03/ex02/i_got_that.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
 msg = input("What you gotta say? : ")
+if msg == "STOP":
+    exit()
 
-while msg != "STOP":
+while True:
     msg = input("I got that! Anything else? : ")
+    if msg == "STOP":
+        break
